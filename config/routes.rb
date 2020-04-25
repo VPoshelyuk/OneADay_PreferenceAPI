d99a5843836f68b39e3226851204e99a79630bef
VPoshelyuk <aslava198@gmail.com>
2020-04-25 14:02:06 -0400

    routes check

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get "/movies", to: "movies#index"
+      get "/tvshows", to: "movies#index"
+      get "/games", to: "movies#index"
       # resources :movies
-      resources :shows
-      resources :games
+      # resources :shows
+      # resources :games
     end
   end
 end
